cadced0f9f6585cc0f56106e884a5ea16f823d4f
nabiha-77bit <nadnan.bscs24seecs@seecs.edu.pk>
2025-04-28 17:28:07 +0500

    Crime class added

--- src/main/java/com/crimeinvestigation/system/model/Crime.java
+++ src/main/java/com/crimeinvestigation/system/model/Crime.java
@@ -1,4 +1,18 @@
 package com.crimeinvestigation.system.model;
 
 public class Crime {
+    int crimeID;
+    boolean CCTVpresence;
+    String  weaponUsed;
+     String injuryDetail;
+     String motive ;
+
+
+ Crime(int crimeID,boolean CCTVpresence,String weaponUsed, String injuryDetail,String motive) {
+     this.crimeID=crimeID;
+     this.CCTVpresence=CCTVpresence;
+     this.weaponUsed=weaponUsed;
+     this.injuryDetail=injuryDetail;
+     this.motive=motive;
+ }
 }
--- src/main/java/com/crimeinvestigation/system/model/User.java
+++ src/main/java/com/crimeinvestigation/system/model/User.java
@@ -1,15 +1,22 @@
 package com.crimeinvestigation.system.model;
 
+
 public class User extends Person{
+    public enum UserType {
+        Victim,
+        Eyewtiness
+    }
     protected String Address;
     protected String email;
+protected UserType UserType;
 
 
-    User(String name,String FatherName,String contactNumber,String DOB, String Occupation,String CNIC,int PersonID,String Gender){
+    User(String name,String FatherName,String contactNumber,String DOB, String Occupation,String CNIC,int PersonID,String Gender,String Address,
+         String email,UserType UserType){
         super(name,FatherName,contactNumber,DOB,Occupation,CNIC,PersonID,Gender);
         this.Address=Address;
         this.email=email;
-
+this.UserType=UserType;
 
     }
 }
