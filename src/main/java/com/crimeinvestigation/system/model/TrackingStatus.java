3d0b105ca1b0c89ce18b77a05956ba88f2d2ed9f
mahammansoor <mmansoor.bscs24seecs@seecs.edu.pk>
2025-04-28 17:10:52 +0500

    TrackingStatus class added

--- src/main/java/com/crimeinvestigation/system/model/TrackingStatus.java
+++ src/main/java/com/crimeinvestigation/system/model/TrackingStatus.java
@@ -1,4 +1,43 @@
 package com.crimeinvestigation.system.model;
 
+import java.time.LocalDate;
+
 public class TrackingStatus {
+    private int trackingID;
+    private int caseID;
+    private String updateSatus;
+    private LocalDate updateDate;
+
+    TrackingStatus(int trackingID,int caseID,String updateSatus,LocalDate updateDate){
+        this.trackingID=trackingID;
+        this.caseID=caseID;
+        this.updateSatus=updateSatus;
+        this.updateDate=updateDate;
+    }
+    //getters and setters
+    public int getTrackingID() {
+        return trackingID;
+    }
+    public void setTrackingID(int trackingID) {
+        this.trackingID = trackingID;
+    }
+    public int getCaseID() {
+        return caseID;
+    }
+    public void setCaseID(int caseID) {
+        this.caseID = caseID;
+    }
+    public String getUpdateSatus() {
+        return updateSatus;
+    }
+    public void setUpdateSatus(String updateSatus) {
+        this.updateSatus = updateSatus;
+    }
+    public LocalDate getUpdateDate() {
+        return updateDate;
+    }
+    public void setUpdateDate(LocalDate updateDate) {
+        this.updateDate = updateDate;
+    }
+
 }
