0d0d340aaaad14ef6cd9c5eaab583ff4a61ddadd
nabiha-77bit <nadnan.bscs24seecs@seecs.edu.pk>
2025-04-29 12:38:59 +0500

    get/setters added in crime class

--- src/main/java/com/crimeinvestigation/system/model/Crime.java
+++ src/main/java/com/crimeinvestigation/system/model/Crime.java
@@ -15,4 +15,14 @@ public class Crime {
      this.injuryDetail=injuryDetail;
      this.motive=motive;
  }
+ public void setcrimeID(int crimeID) { this.crimeID=crimeID; }
+ public int getcrimeID() { return crimeID; }
+    public void setCCTVpresence(boolean CCTVpresence) { this.CCTVpresence=CCTVpresence; }
+    public boolean getCCTVpresence() { return CCTVpresence; }
+    public void setweaponUsed(String weaponUsed) { this.weaponUsed=weaponUsed; }
+    public String getweaponUsed() { return weaponUsed;}
+    public void setInjuryDetail(String injuryDetail) {this.injuryDetail = injuryDetail;}
+    public String getInjuryDetail() { return injuryDetail;}
+    public void setMotive(String motive) { this.motive=motive; }
+    public String getMotive() { return motive;}
 }
