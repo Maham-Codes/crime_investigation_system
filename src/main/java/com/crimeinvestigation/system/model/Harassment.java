c8e4b7860b96cd7f9a5fd398b3086db18607a836
sumayya-emaan <semaan.bscs24seecs@seecs.edu.pk>
2025-04-29 17:15:15 +0500

    harassment abstract class

---
+++ src/main/java/com/crimeinvestigation/system/model/Harassment.java
@@ -0,0 +1,11 @@
+package com.crimeinvestigation.system.model;
+
+public abstract class Harassment extends Crime{
+protected String harassmentType;
+
+public Harassment(int CrimeID,boolean CCTVpresence,String weaponUsed,String injuryDetail,String motive,String harassmentType){
+    super(CrimeID,CCTVpresence,weaponUsed,injuryDetail,motive);
+    this.harassmentType=harassmentType;
+}
+public abstract String harassmentDetail();
+}
