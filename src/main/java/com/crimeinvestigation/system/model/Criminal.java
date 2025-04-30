ff79c32852e84b8472c49cec390b0c229b44b239
mahammansoor <mmansoor.bscs24seecs@seecs.edu.pk>
2025-04-30 15:16:56 +0500

    Criminal methods added

--- src/main/java/com/crimeinvestigation/system/model/Criminal.java
+++ src/main/java/com/crimeinvestigation/system/model/Criminal.java
@@ -17,6 +17,33 @@ public class Criminal extends Person {
           this.Nationality=Nationality;
 
      }
+     public String getCrimeType() { return CrimeType; }
+     public void setCrimeType(String crimeType) { this.CrimeType = crimeType; }
+     public String getCurrentStatus() { return CurrentStatus; }
+     public void setCurrentStatus(String currentStatus) { this.CurrentStatus = currentStatus; }
+     public String getReputation() { return Reputation; }
+     public void setReputation(String reputation) { this.Reputation = reputation; }
+     public String getNationality() { return Nationality; }
+     public void setNationality(String nationality) { this.Nationality = nationality; }
+
+     public void updateStatus(String newStatus) {
+          this.CurrentStatus = newStatus;
+     }
+
+     public String getDetails() {
+          return  "Name: " + getName() + "\n" +
+                  "Father's Name: " + getFatherName() + "\n" +
+                  "CNIC: " + getCNIC() + "\n" +
+                  "Gender: " + getGender() + "\n" +
+                  "Date of Birth: " + getDOB() + "\n" +
+                  "Occupation: " + getOccupation() + "\n" +
+                  "Contact Number: " + getContactNumber() + "\n" +
+                  "Person ID: " + getPersonID() + "\n" +
+                  "Crime Type: " + CrimeType + "\n" +
+                  "Current Status: " + CurrentStatus + "\n" +
+                  "Reputation: " + Reputation + "\n" +
+                  "Nationality: " + Nationality;
+     }
 
 
 }
