7706fb9adee5dfd785c7caa6a3d819a5e795e7c5
sumayya-emaan <semaan.bscs24seecs@seecs.edu.pk>
2025-05-02 18:31:32 +0500

    theft class

---
+++ src/main/java/com/crimeinvestigation/system/model/Murder.java
@@ -0,0 +1,63 @@
+package com.crimeinvestigation.system.model;
+
+import java.sql.SQLOutput;
+import java.time.LocalDateTime;
+
+public class Murder extends Crime{
+    private LocalDateTime timeOfDeath;
+    private String victimName;
+    private boolean bodyRecovered;
+    private String autopsy;
+
+    public Murder(LocalDateTime timeOfDeath, String victimName, boolean bodyRecovered, String autopsy) {
+        super();
+        this.timeOfDeath = timeOfDeath;
+        this.victimName = victimName;
+        this.bodyRecovered = bodyRecovered;
+        this.autopsy = autopsy;
+    }
+
+    public Murder(int crimeID, boolean CCTVpresence, String weaponUsed, String injuryDetail, String motive, String CrimeType, LocalDateTime timeOfDeath, String victimName, boolean bodyRecovered, String autopsy) {
+        super(crimeID, CCTVpresence, weaponUsed, injuryDetail, motive, CrimeType);
+        this.timeOfDeath = timeOfDeath;
+        this.victimName = victimName;
+        this.bodyRecovered = bodyRecovered;
+        this.autopsy = autopsy;
+    }
+
+    public LocalDateTime getTimeOfDeath() {
+        return timeOfDeath;
+    }
+
+    public void setTimeOfDeath(LocalDateTime timeOfDeath) {
+        this.timeOfDeath = timeOfDeath;
+    }
+
+    public String getVictimName() {
+        return victimName;
+    }
+
+    public void setVictimName(String victimName) {
+        this.victimName = victimName;
+    }
+
+    public boolean isBodyRecovered() {
+        return bodyRecovered;
+    }
+
+    public void setBodyRecovered(boolean bodyRecovered) {
+        this.bodyRecovered = bodyRecovered;
+    }
+
+    public String getAutopsy() {
+        return autopsy;
+    }
+
+    public void setAutopsy(String autopsy) {
+        this.autopsy = autopsy;
+    }
+
+    public void causeOfDeath(String autopsy){
+        System.out.println("The cause of death can be known in detail from the autopsy report which is given in detail below: "+autopsy);
+    }
+}
