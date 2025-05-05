fa902e87423835e90d8972b610d4d5225b0f7ccf
mahammansoor <mmansoor.bscs24seecs@seecs.edu.pk>
2025-05-05 18:02:06 +0500

    Added backend directory

--- src/main/java/com/crimeinvestigation/system/model/CrimeType.java
+++
@@ -1,6 +0,0 @@
-package com.crimeinvestigation.system.model;
-
-@FunctionalInterface
-public interface CrimeType {
-    public void displayDetails();
-}
--- src/main/java/com/crimeinvestigation/system/repository/LawsAndPunishmentsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.crimeinvestigation.system.repository;
-
-import com.crimeinvestigation.system.model.LawsAndPunishments;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LawsAndPunishmentsRepository extends JpaRepository<LawsAndPunishments, String> {
-    // Basic CRUD operations are available
-}
