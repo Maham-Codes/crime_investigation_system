cfd820a4521f44c268f2460fa19b1f8972b7990c
mahammansoor <mmansoor.bscs24seecs@seecs.edu.pk>
2025-05-06 18:42:10 +0500

    Database connection errors

--- backend/src/main/java/com/crimeinvestigation/system/DatabaseConnection.java
+++ backend/src/main/java/com/crimeinvestigation/system/DatabaseConnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/cia";
-    private static final String USER = "root";
-    private static final String PASSWORD = "#4July2005#";
+    private static final String USER = "cia_user";
+    private static final String PASSWORD = "crime123";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
