51b8899bacf780d74f777958b00aaf5ce1d30f82
mahammansoor <mmansoor.bscs24seecs@seecs.edu.pk>
2025-05-09 02:07:05 +0500

    mujhe ronaa haii

--- backend/src/main/java/com/crimeinvestigation/system/CrimeInvestigationSystemApplication.java
+++ backend/src/main/java/com/crimeinvestigation/system/CrimeInvestigationSystemApplication.java
@@ -1,13 +1,47 @@
+//package com.crimeinvestigation.system;
+//
+//import com.crimeinvestigation.system.dao.CrimeTypeDao;
+//import com.crimeinvestigation.system.model.CrimeType;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//import java.sql.*;
+//import java.util.*;
+//
+//@SpringBootApplication
+//public class CrimeInvestigationSystemApplication {
+//
+//    public CrimeInvestigationSystemApplication() throws SQLException {
+//        CrimeTypeDao crimeTypeDao = null;
+//        List<CrimeType> types = crimeTypeDao.getAllCrimeTypes();
+//        types.forEach(type -> System.out.println("TypeID: " + type.getTypeID() + ", Name: " + type.getCrimeName()));
+//    }
+//
+//
+//}
 package com.crimeinvestigation.system;
 
+import com.crimeinvestigation.system.dao.CrimeTypeDao;
+import com.crimeinvestigation.system.model.CrimeType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.List;
 
 @SpringBootApplication
-public class CrimeInvestigationSystemApplication {
+public class CrimeInvestigationSystemApplication implements CommandLineRunner {
+
+    @Autowired
+    private CrimeTypeDao crimeTypeDao;
 
     public static void main(String[] args) {
         SpringApplication.run(CrimeInvestigationSystemApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        List<CrimeType> types = crimeTypeDao.getAllCrimeTypes();
+        types.forEach(type -> System.out.println("TypeID: " + type.getTypeID() + ", Name: " + type.getCrimeName()));
+    }
 }
--- backend/src/main/java/com/crimeinvestigation/system/dao/CrimeTypeDao.java
+++ backend/src/main/java/com/crimeinvestigation/system/dao/CrimeTypeDao.java
@@ -1,29 +1,70 @@
+//package com.crimeinvestigation.system.dao;
+//
+//import com.crimeinvestigation.system.DatabaseConnection;
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//
+//public class CrimeTypeDao {
+//    public void getAllCrimeTypes() {
+//        String sql = "SELECT * FROM crimetype";
+//
+//        try (Connection conn = DatabaseConnection.getConnection();
+//             PreparedStatement stmt = conn.prepareStatement(sql);
+//             ResultSet rs = stmt.executeQuery()) {
+//
+//            System.out.println("=== Crime Types in Database ===");
+//
+//            while (rs.next()) {
+//                int id = rs.getInt("typeID");
+//                String name = rs.getString("crimeName");
+//                System.out.println("Type ID: " + id + ", Crime Name: " + name);
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
+
 package com.crimeinvestigation.system.dao;
 
+import com.crimeinvestigation.system.model.CrimeType;
 import com.crimeinvestigation.system.DatabaseConnection;
+import org.springframework.stereotype.Repository;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+@Repository
 public class CrimeTypeDao {
-    public void getAllCrimeTypes() {
+
+    public List<CrimeType> getAllCrimeTypes() {
+        List<CrimeType> crimeTypes = new ArrayList<>();
         String sql = "SELECT * FROM crimetype";
 
         try (Connection conn = DatabaseConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql);
              ResultSet rs = stmt.executeQuery()) {
 
-            System.out.println("=== Crime Types in Database ===");
-
             while (rs.next()) {
-                int id = rs.getInt("typeID");
-                String name = rs.getString("crimeName");
-                System.out.println("Type ID: " + id + ", Crime Name: " + name);
+                CrimeType crimeType = new CrimeType();
+                crimeType.setTypeID(rs.getInt("typeID"));
+                crimeType.setCrimeName(rs.getString("crimeName"));
+                crimeTypes.add(crimeType);
             }
 
         } catch (SQLException e) {
+            System.err.println("Error retrieving crime types:");
             e.printStackTrace();
         }
+
+        return crimeTypes;
     }
 }
+
