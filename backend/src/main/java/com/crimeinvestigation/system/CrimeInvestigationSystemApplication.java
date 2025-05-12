847c2574c616ff8976c70d88f7881dfa0e563a68
sumayya-emaan <semaan.bscs24seecs@seecs.edu.pk>
2025-05-12 23:11:22 +0500

    error commented

--- backend/src/main/java/com/crimeinvestigation/system/CrimeInvestigationSystemApplication.java
+++ backend/src/main/java/com/crimeinvestigation/system/CrimeInvestigationSystemApplication.java
@@ -1,5 +1,6 @@
 package com.crimeinvestigation.system;
 
+import com.crimeinvestigation.system.dao.*;
 import com.crimeinvestigation.system.service.CrimeTypeService;
 import com.crimeinvestigation.system.service.CaseTrackingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import java.util.List;
+import java.util.Map;
+
 @SpringBootApplication
 @EntityScan(basePackages = "com.crimeinvestigation.system.model")
 public class CrimeInvestigationSystemApplication implements CommandLineRunner {
@@ -15,15 +19,23 @@ public class CrimeInvestigationSystemApplication implements CommandLineRunner {
     @Autowired
     private CrimeTypeService crimeTypeService;
 
+    @Autowired
+    private CrimeCaseDao crimeCaseDao;
 
 
-    public static void main(String[] args) {
-        SpringApplication.run(CrimeInvestigationSystemApplication.class, args);
+   /* public static void main(String[] args) {
+        SpringApplication.run(CrimeInvestigationApplication.class, args);
     }
 
+*/
     @Override
     public void run(String... args) {
-        crimeTypeService.displayAllCrimeTypes();
 
+        // Call the CrimeCaseDao method to get closed cases and print them
+       /* List<Map<String, Object>> closedCases = crimeCaseDao.getClosedCases();
+        for (Map<String, Object> row : closedCases) {
+            System.out.println(row);
+        }
+*/
     }
 }
